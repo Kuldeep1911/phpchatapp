33b3e87dcd43789436661be25a6aecc10e9dd93b
Kuldeep1911 <114897051+Kuldeep1911@users.noreply.github.com>
2024-12-22 13:48:10 +0530

    Update config.php

--- phpChatApp/php/config.php
+++ phpChatApp/php/config.php
@@ -1,6 +1,6 @@
 <?php  
  
- $conn = mysqli_connect("localhost","root","","chatapp");
+ $conn = mysqli_connect("localhost","verceldb","wESRFh8bM9JD","chatapp");
  if(!$conn){
     echo "Database connection error".mysqli_connect_error();
   }
